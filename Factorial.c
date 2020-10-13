cba38079f073153c99b829e4995abea0661c4f6d
Mihir Gupta <mihirgupta84@gmail.com>
2020-10-13 18:12:44 +0530

    Updated Factorial of a number problem
    Added a condition for a negative number

--- Factorial.c
+++ Factorial.c
@@ -1,17 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-  int i,fac=1,num;
-  cout<<"Enter number of which u want to calculate factorial";
-  cin>>num;
-
-    for(i=1;i<=num;i++)
-    {
-      fac=fac*i;
-    }
-
-   cout<<"factorial of " <<num<< "is:" <<fac<<endl;
-
-  return 0;
+  	int i,fac=1,num;
+  	cout<<"Enter number of which u want to calculate factorial";
+  	cin>>num;
+	if(num<0)
+	{
+		cout<<"A negative number doesnt have any factorial !";
+	}
+	else
+	{
+		for(i=1;i<=num;i++)
+   	 	{
+  	    	fac=fac*i;
+ 	   	}
+   		cout<<"factorial of " <<num<< "is:" <<fac<<endl;
+	}
+  	return 0;
 }
