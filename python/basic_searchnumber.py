dc7edf8d5dc27937028f13134c0cc9f968a6f7b3
Prajit Adhikari <prajit.raj.adhikari@gmail.com>
2020-10-14 09:09:29 +0545

    Improved readability and reduced unnecessary commands..

--- python/basic_searchnumber.py
+++ python/basic_searchnumber.py
@@ -1,20 +1,16 @@
-l=[]
-print("Total number elements you want to add:")
-t=int(input())
+# Initialization of new list, can also be done by l = list()
+l = []
+t = int(input("Total number elements you want to add:"))
 print("input elements:")
 
-for j in range(0,t):
-    p=int(input())
-    l.append(p) 
-print("Unsorted list is:")        
-print(l)  
-print("Sorted list is:")
+for j in range(0, t):
+    p = int(input())
+    l.append(p)
+print("Unsorted list is:", l, sep="")
 l.sort()
-print(l)
-print("Enter the number you want to find index of :")
-n=int(input())
+print("Sorted list is:", l, sep="")
+n = int(input("Enter the number you want to find index of :"))
 if n in l:
-  print("number is at index:")
-  print(l.index(n))
+    print("number is at index:", l.index(n), sep="")
 else:
-    print("element does not exits")
+    print("element does not exist")
