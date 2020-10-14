ecbb6e027921e5af4f70990bc7d1613b04714085
Prajit Adhikari <prajit.raj.adhikari@gmail.com>
2020-10-14 10:30:44 +0545

    Reformatted code, reorganized dir, and redundancy.

--- python/basic_searchnumber.py
+++ python/basic_searchnumber.py
@@ -1,16 +1,17 @@
 # Initialization of new list, can also be done by l = list()
-l = []
+list1 = []
 t = int(input("Total number elements you want to add:"))
 print("input elements:")
 
 for j in range(0, t):
     p = int(input())
-    l.append(p)
-print("Unsorted list is:", l, sep="")
-l.sort()
-print("Sorted list is:", l, sep="")
+    list1.append(p)
+print("Unsorted list is:", list1, sep="")
+# Permanently sorting the list
+list1.sort()
+print("Sorted list is:", list1, sep="")
 n = int(input("Enter the number you want to find index of :"))
-if n in l:
+if n in list1:
     print("number is at index:", l.index(n), sep="")
 else:
     print("element does not exist")
