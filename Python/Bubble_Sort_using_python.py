5fa4c6e8f96215f9bdcde0147b278c48041bb8af
ARAVIND R K <48731841+boun7yhunt3r@users.noreply.github.com>
2020-10-21 22:19:36 +0530

    Update Bubble_Sort_using_python.py
    updated method

--- Python/Bubble_Sort_using_python.py
+++ Python/Bubble_Sort_using_python.py
@@ -1,11 +1,9 @@
 def bubbleSort(arr):
-    for passnum in range(len(arr)-1,0,-1):
-        for i in range(passnum):
-            if arr[i]>arr[i+1]:
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
-
+    for i in range(len(arr)): 
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j]                 
+                
 n=int(input("Give range of list of numbers"))
 arr=[]
 for i in range(n):
